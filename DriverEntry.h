f7dbaff78f4411bfe47679e9606799720028fdcc
Muhammad <122938837+xamurani@users.noreply.github.com>
2025-05-24 19:40:57 +0500

    Update DriverEntry.h

--- DriverEntry.h
+++ DriverEntry.h
@@ -2,7 +2,9 @@
 #include "kpdb.h"
 #include "ntutils.h"
 
-#define MAX_SYMBOL_DATA 32
+#define MAX_SYMBOL_DATA 32 // This is already in kpdb.h, can be removed here if kpdb.h always included first.
+                           // Or ensure it matches if defined in both.
 
-void KpdbDemoRoutine();
-NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegistryPath);
+// Functions are now part of kpdb.h as well. Redundant here if kpdb.h is included.
+// void KpdbDemoRoutine();
+// NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegistryPath);
